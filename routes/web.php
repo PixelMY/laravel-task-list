17919a988e136ad67bf82e9f7b3abc90053a743b
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-03 12:38:30 +0800

    16. All About Routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+
+// GET
+// POST 
+// PUT 
+// DELETE 
