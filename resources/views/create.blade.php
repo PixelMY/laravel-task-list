8ba7e7c40b526369af684911a9a026a6a71c1d0f
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-04 10:38:42 +0800

    Keeping Old Values in the Form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -19,7 +19,7 @@
 
 		<div>
 			<label for="title">Title</label>
-			<input type="text" name="title" id="title">
+			<input type="text" name="title" id="title" value="{{ old("title") }}">
 
 			@error("title")
 				<p class="error-message"> {{ $message }} </p>
@@ -28,7 +28,7 @@
 
 		<div>
 			<label for="description">Description</label>
-			<textarea name="description" id="description" rows="2"></textarea>
+			<textarea name="description" id="description" rows="2">{{ old("description") }}</textarea>
 
 			@error("description")
 				<p class="error-message"> {{ $message }} </p>
@@ -37,7 +37,7 @@
 
 		<div>
 			<label for="description">Long Description</label>
-			<textarea name="long_description" id="long_description" rows="10"></textarea>
+			<textarea name="long_description" id="long_description" rows="10">{{ old("long_description") }}</textarea>
 
 			@error("long_description")
 				<p class="error-message"> {{ $message }} </p>
