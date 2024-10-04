2471fd2f3efffadb72f561d070d8959b89b40d76
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-04 11:30:08 +0800

    Deleting Data

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,4 +12,13 @@
 	<p>{{ $task->created_at }}</p>
 
 	<p>{{ $task->updated_at }}</p>
+
+	<div>
+		<form action="{{ route("tasks.destroy", ["task" => $task->id]) }}" method="post">
+			@csrf
+			@method("DELETE")
+
+			<button type="submit">Delete</button>
+		</form>
+	</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -140,6 +140,14 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
 
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success','Task deleted successfully!');
+
+})->name('tasks.destroy');
+
 // Route::get('/xxx', function () {
 //     return 'Hello';
 // })->name('hello');
