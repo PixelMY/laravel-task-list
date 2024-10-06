4b9bdfbba42397c5927b41f08659dd97c95e6d7f
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-06 21:37:46 +0800

    Adding Styling with Tailwind CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,28 @@
 @section("title", "The list of tasks")
 
 @section("content")
-	<div>
-		<div>
-			<a href="{{ route("tasks.create") }}">Add Task!</a>
-		</div>
-		{{-- @if (count($tasks)) --}}
-		@forelse ($tasks as $task)
-			{{-- <div>{{ $task->title }}</div> --}}
-			<div>
-				<a href="{{ route("tasks.show", ["task" => $task->id]) }}">{{ $task->title }}</a>
-			</div>
-		@empty
-			<div>There are no tasks!</div>
-		@endforelse
-		{{-- @endif --}}
+	{{-- <div> --}}
+	{{-- <div> --}}
+	<nav class="mb-4">
+		<a class="font-medium text-gray-700 underline decoration-pink-500" href="{{ route("tasks.create") }}">Add Task!</a>
+	</nav>
 
-		@if ($tasks->count())
-			<nav>
-				{{ $tasks->links() }}
-			</nav>
-		@endif
-	</div>
+	{{-- </div> --}}
+	{{-- @if (count($tasks)) --}}
+	@forelse ($tasks as $task)
+		{{-- <div>{{ $task->title }}</div> --}}
+		<div>
+			<a @class(["line-through" => $task->completed]) href="{{ route("tasks.show", ["task" => $task->id]) }}">{{ $task->title }}</a>
+		</div>
+	@empty
+		<div>There are no tasks!</div>
+	@endforelse
+	{{-- @endif --}}
+
+	@if ($tasks->count())
+		<nav class="mt-4">
+			{{ $tasks->links() }}
+		</nav>
+	@endif
+	{{-- </div> --}}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Laravel Task List App</title>
+	<script src="https://cdn.tailwindcss.com"></script>
 	@yield("styles")
 </head>
 
-<body>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
 
-	<h1>@yield("title")</h1>
+	<h1 class="text-2xl mb-4">@yield("title")</h1>
 
 	<div>
 		@if (session()->has("success"))
