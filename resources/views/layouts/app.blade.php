9129f5cdbeace4ee8f781b2423a27814642cc36d
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-06 22:20:54 +0800

    Styling Flash Message and Using Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Laravel Task List App</title>
 	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="//unpkg.com/alpinejs" defer></script>
 	{{-- blade-formatter-disable --}}
 	<style type="text/tailwindcss">
 	
@@ -37,9 +38,21 @@
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
 	<h1 class="text-2xl mb-4">@yield("title")</h1>
-	<div>
+	<div x-data="{ flash: true }">
 		@if (session()->has("success"))
-			<div>{{ session("success") }} </div>
+			{{-- <div>{{ session("success") }}</div> --}}
+			<div x-show="flash"
+				class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+				<strong class="font-bold">Success!</strong>
+				<div>{{ session("success") }}</div>
+
+				<span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+						@click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</span>
+			</div>
 		@endif
 		@yield("content")
 	</div>
